58b701c1b0841d9148c674a6792af9a3638f6510
shotakawasaki0806 <hanshintigers.gofothetop@gmail.com>
2021-01-06 12:54:47 +0900

    単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {"輝明"}
+    last_name             {"佐藤"}
+    first_kana            {"テルアキ"}
+    last_kana             {"サトウ"}
+    birthday              {"1992-08-06"}
+  end
+end
