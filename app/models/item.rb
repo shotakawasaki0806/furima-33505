27ddf524246107168deaa912a40232e25b137f30
shotakawasaki0806 <hanshintigers.gofothetop@gmail.com>
2021-01-07 12:35:17 +0900

    商品出品機能実装

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :area
+  belongs_to_active_hash :wait
+
+  with_options presence: true do
+  validates :product
+  validates :explanation 
+  validates :image
+  validates :price, format: {with: /\d/}
+  validates :price, numericality: {greater_than_or_equal_to:300, less_than_or_equal_to:9999999}
+  end
+  validates :category_id, :state_id, :postage_id, :area_id, :wait_id, numericality: { other_than: 1 }
+end
