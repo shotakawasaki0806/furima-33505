69ad94efa865ab10932cbfa0b479f28255645b55
shotakawasaki0806 <hanshintigers.gofothetop@gmail.com>
2021-01-08 15:33:50 +0900

    before_actionの整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,16 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -41,9 +39,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product, :price, :explanation, :image, :category_id, :state_id, :postage_id, :area_id, :wait_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
