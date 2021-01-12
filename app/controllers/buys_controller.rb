3fa6c0f3da207e8b342776b70005836bf0f85218
shotakawasaki0806 <hanshintigers.gofothetop@gmail.com>
2021-01-12 12:35:12 +0900

    before_actionの追加

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,15 +1,14 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
   before_action :move_to_root
   before_action :move_to_out
 
   def index
-    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(address_params)
     if @buy_address.valid?
       pay_item
@@ -23,6 +22,10 @@ class BuysController < ApplicationController
   private
   def address_params
     params.require(:buy_address).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
